96651777bc60a26ed81fa5bed40ec7c71bc1ac56
J_W_I_ <64947174+JWIMaster@users.noreply.github.com>
2025-10-07 14:41:38 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,63 +1,70 @@
-// swift-tools-version:4.2
-
+// swift-tools-version:5.0
 import PackageDescription
 
-#if os(macOS) // This fires for both macOS and iOS targets, because it's based on build platform
+#if os(macOS)
 let platformDependencies: [Package.Dependency] = []
 let platformExcludes = ["Linux", "Operations/Shaders"]
 let platformTargets: [Target] = [
-        .target(
-            name: "GPUImage",
-            path: "framework/Source",
-            exclude: platformExcludes)]
-let platformProducts: [Product] =  [
-        .library(
-            name: "GPUImage",
-            targets: ["GPUImage"]),
-    ]
+    .target(
+        name: "GPUImage",
+        path: "framework/Source",
+        exclude: platformExcludes
+    )
+]
+let platformProducts: [Product] = [
+    .library(
+        name: "GPUImage",
+        targets: ["GPUImage"]
+    )
+]
 #elseif os(Linux)
-// TODO: Add back in RPi support
-// TODO: Move the remote system library packages into this project
 let platformDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/twostraws/SwiftGD.git", .exact("2.3.0")),
-    .package(url: "https://github.com/BradLarson/COpenGL.git", from: "1.0.2"), 
-    .package(url: "https://github.com/BradLarson/CFreeGLUT.git", from: "1.0.1"), 
-    .package(url: "https://github.com/BradLarson/CVideo4Linux.git", from: "1.0.2")]
-let platformExcludes =  ["Apple", "Operations/Shaders", "Linux/RPiRenderWindow.swift", "Linux/OpenGLContext-RPi.swift", "Linux/V4LSupplement", "Linux/V4LCamera"]
+    .package(url: "https://github.com/BradLarson/COpenGL.git", from: "1.0.2"),
+    .package(url: "https://github.com/BradLarson/CFreeGLUT.git", from: "1.0.1"),
+    .package(url: "https://github.com/BradLarson/CVideo4Linux.git", from: "1.0.2")
+]
+let platformExcludes = ["Apple", "Operations/Shaders", "Linux/RPiRenderWindow.swift", "Linux/OpenGLContext-RPi.swift", "Linux/V4LSupplement", "Linux/V4LCamera"]
 let platformTargets: [Target] = [
-        .target(
-            name: "lodepng",
-            path: "framework/Packages/lodepng"),
-        .target(
-            name: "GPUImage",
-            dependencies: ["SwiftGD", "lodepng"],
-            path: "framework/Source",
-            exclude: platformExcludes),
-        .target(
-            name: "V4LSupplement",
-            path: "framework/Source/Linux/V4LSupplement"),
-        .target(
-            name: "GPUImageV4LCamera",
-            dependencies: ["GPUImage", "V4LSupplement"],
-            path: "framework/Source/Linux/V4LCamera")]
-let platformProducts: [Product] =  [
-        .library(
-            name: "GPUImage",
-            targets: ["GPUImage"]),
-        .library(
-            name: "GPUImageV4LCamera",
-            targets: ["GPUImageV4LCamera"]),
-    ]
+    .target(
+        name: "lodepng",
+        path: "framework/Packages/lodepng"
+    ),
+    .target(
+        name: "GPUImage",
+        dependencies: ["SwiftGD", "lodepng"],
+        path: "framework/Source",
+        exclude: platformExcludes
+    ),
+    .target(
+        name: "V4LSupplement",
+        path: "framework/Source/Linux/V4LSupplement"
+    ),
+    .target(
+        name: "GPUImageV4LCamera",
+        dependencies: ["GPUImage", "V4LSupplement"],
+        path: "framework/Source/Linux/V4LCamera"
+    )
+]
+let platformProducts: [Product] = [
+    .library(
+        name: "GPUImage",
+        targets: ["GPUImage"]
+    ),
+    .library(
+        name: "GPUImageV4LCamera",
+        targets: ["GPUImageV4LCamera"]
+    )
+]
 #endif
-
 
 let package = Package(
     name: "GPUImage",
-    products: platformProducts,
     platforms: [
-        .iOS("7.0") // set minimum iOS you want to support
+        .iOS("7.0")
     ],
+    products: platformProducts,
     dependencies: platformDependencies,
     targets: platformTargets,
-    swiftLanguageVersions: [.v4]
+    swiftLanguageVersions: [.v4, .v5]
 )
